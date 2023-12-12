b936ffa2fb4878f2c394ec77a47bc4d6414bcae1
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-12-11 20:25:36 -0600

    Assignment1

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -75,7 +72,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -95,7 +92,20 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            for(Map.Entry<String, String> entry: row.entrySet()) {
+
+                String aValue = entry.getValue();
+
+                if (aValue.toLowerCase().contains(value)) {
+                    jobs.add(row);
+                }
+            }
+        }
+
+        return jobs;
     }
 
     /**
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +112,33 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.size() == 0) {
+            System.out.print("No Results");
+        }
+        for (HashMap<String, String> job : someJobs) {
+            System.out.println("\n" + "*****" +
+                    "\nposition type: " + job.get("position type") +
+                    "\nname: " + job.get("name") +
+                    "\nemployer: " + job.get("employer") +
+                    "\nlocation: " + job.get("location") +
+                    "\ncore competency: " + job.get("core competency") +
+                    "\n" + "*****");
 
-        System.out.println("printJobs is not implemented yet");
+        }
+
     }
 }
+
+
+
+
+
+
+
