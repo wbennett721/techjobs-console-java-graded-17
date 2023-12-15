b987f36a1460372bb4e454720de85f7b53a57b53
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-12-14 18:14:28 -0600

    Assignment1

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -116,25 +116,25 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        if (someJobs.isEmpty()) {
+        if (someJobs.size() == 0) {
             System.out.print("No Results");
+            return;
         }
-        else {
-        for (HashMap<String, String> job : someJobs) {
-            System.out.println("\n" + "*****" +
-                    "\nposition type: " + job.get("position type") +
-                    "\nname: " + job.get("name") +
-                    "\nemployer: " + job.get("employer") +
-                    "\nlocation: " + job.get("location") +
-                    "\ncore competency: " + job.get("core competency") +
-                    "\n" + "*****");
+
+
+        for(HashMap<String, String> job : someJobs) {
+            System.out.println("\n*****");
+            for(Map.Entry<String,String> jobColumn : job.entrySet()) {
+                System.out.println(jobColumn.getKey() + ": " + jobColumn.getValue());
+            }
+            System.out.println("*****");
 
         }
         }
 
     }
 
-}
+
 
 
 
